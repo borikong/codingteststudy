dae6d5f936854a90ff5895e1f65640f1c8848957
dayeong <da1599@naver.com>
2022-08-08 14:07:26 +0900

    0808

--- bj/11053 가장 긴 증가하는 부분 수열.py
+++ bj/11053 가장 긴 증가하는 부분 수열.py
@@ -1,7 +1,18 @@
 import sys
 
-##정렬이 안된 상태에서
 N=int(sys.stdin.readline())
-A=map(int,sys.stdin.readline().split())
-dp=[]
+A=list(map(int,sys.stdin.readline().split()))
+dp=[0 for _ in range(N)]
+dp[0]=0
+dp[1]=1
 
+for i in range(2, N):
+    for j in range(A.index(i)):
+        if a[]
+
+# 7
+# 1 4 5 2 3 4 5
+
+#1.min 부터 출발해야 한다?? -> 그것도 아님
+
+# print(dp)
